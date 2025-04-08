2bcd974b8677f5f98594737155a7b89753ebe673
ShotaGordadze <sh.gordadze@rsig.ge>
2025-04-08 15:00:18 +0400

    implementing supabase

---
+++ TaskMonitoring/Infrastructure/SupabaseService.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+using Supabase;
+
+namespace Infrastructure;
+
+public class SupabaseService
+{
+    private string _supabaseUrl;
+    private string _supabaseKey;
+    private readonly string _path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Settings.json");
+
+    private Client _client;
+
+    public SupabaseService()
+    {
+        Load(_path);
+        InitializeAsync();
+    }
+
+     private async Task InitializeAsync()
+    {
+        var options = new SupabaseOptions
+        {
+            AutoConnectRealtime = false
+        };
+
+        _client = new Client(
+            _supabaseUrl,
+            _supabaseKey,
+            options);
+
+        await _client.InitializeAsync();
+    }
+
+    private void Load(string path)
+    {
+        var json = File.ReadAllText(path);
+        var config = JsonSerializer.Deserialize<SupabaseConfig>(json);
+
+        if (config == null)
+        {
+            throw new ApplicationException("Invalid configuration"); //ToDo
+        }
+
+        _supabaseUrl = config.SupabaseUrl;
+        _supabaseKey = config.SupabaseKey;
+    }
+}
+
+internal class SupabaseConfig
+{
+    internal string SupabaseUrl { get; init; } = null!;
+    internal string SupabaseKey { get; init; } = null!;
+}
